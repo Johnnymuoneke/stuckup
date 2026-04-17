5f41638209b3bd1604706797a26b1cc0ea672bfa
john <chidozie.john.muoneke@gmail.com>
2026-04-17 22:46:33 +0100

    fifth commit

---
+++ src/app/(tabs)/insights.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native"
+
+const insightScreen =()=>{
+    return(
+        <View>
+            <Text>insights</Text>
+        </View>
+    )
+}
+export default insightScreen
---
+++ src/app/(tabs)/planner.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native"
+
+const PlannerScreen =()=>{
+    return(
+        <View>
+            <Text>Planner</Text>
+        </View>
+    )
+}
+export default PlannerScreen
